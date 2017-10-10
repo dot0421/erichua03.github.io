bd5cc8e320a8ee95578a62cfea1ed1324b7ac887
Chaolei Hua <erichua03@gmail.com>
2017-10-09 19:42:29 -0700

    ahaha

---
+++ js/3d.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    var bannerWrap = document.querySelector('.banner-wrap'),
+        banner = bannerWrap.querySelector('#carousel-example-generic');
+        banner.addEventListener('mousemove', function(e){
+            var centerX = banner.offsetLeft + banner.offsetWidth / 2,
+                centerY = banner.offsetTop + banner.offsetHeight / 2;        
+            var deltaX = e.pageX - centerX,
+                deltaY = e.pageY - centerY;
+            var percentageX = deltaX / centerX,
+                percentageY = deltaY / centerY;
+            var deg = 10;
+            this.style.transform = 'rotateX(' + percentageY * -deg + 'deg)' 
+                                    + 'rotateY(' + percentageX * deg + 'deg)';
+        });
+        banner.addEventListener('mouseleave', function(e){
+            this.style.transform = '';
+        })    
+});
